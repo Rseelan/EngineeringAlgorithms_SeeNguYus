7a5f3d48aefc90e377ce23901bc36d6e7643e80c
Andrew Nguyen <76266011+A-H-Nguyen@users.noreply.github.com>
2021-10-12 17:23:50 -0400

    Update card.h

--- project2_flipcards/deck_class/card.h
+++ project2_flipcards/deck_class/card.h
@@ -5,17 +5,21 @@
 
 using namespace std;
 
+// Declaration of the card class. Stores the value and suit of each card in the deck.
 class card
 {
 public:
-	card(int valIn = 0, char suitIn = 'X');
+	// Constructor:
+	card(int valIn = 0, char suitIn = 'X');	// Pass default value and suit in
 
-	void setValue(int x);
-	void setSuit(char x);
+	// Member functions:
+	void setValue(int x);	// Set value manually
+	void setSuit(char x);	// Set suit manually
 
-	int getValue();
-	char getSuit();
+	int getValue();		// Returns value of card
+	char getSuit();		// Returns suit of card
 
+	// Friend function:
 	friend ostream& operator<< (ostream& ostr, const card& c);
 
 private:
@@ -26,39 +30,46 @@ private:
 /****************************Function Implementation***************************/
 
 card::card(int valIn, char suitIn)
+// Constructor sets value and suit of each card.
 {
 	value = valIn;
 	suit = suitIn;
 }
 
 void card::setValue(int x)
+// Allows user to manually set value of a card.
 {
 	value = x;
 }
 
 void card::setSuit(char x)
+// Allows user to manually set suit of a card.
 {
 	suit = x;
 }
 
 int card::getValue()
+// Returns value of a card.
 {
 	return value;
 }
 
 char card::getSuit()
+// Returns suit of a card.
 {
 	return suit;
 }
 
 ostream& operator<< (ostream& ostr, const card& c)
+// Overloading the '<<' operator to be able to cout value and suit.
 {
-	//for some reason the ',' in this line gives an error:
-	//ostr << c.value << ', ' << c.suit << endl;
+	// For some reason the ',' in this line gives an error:
+	// ostr << c.value << ', ' << c.suit << endl;
 	
 	ostr << c.value << c.suit;
 	return ostr;
 }
+
 /*****************************************************************************/
 
 #endif
