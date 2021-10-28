bd4c549ea77ed6aaca71d35924825984e35ae5b4
Andrew Nguyen <76266011+A-H-Nguyen@users.noreply.github.com>
2021-10-28 18:21:48 -0400

    Add files via upload

--- project3_wordsearch/test.cpp
+++ project3_wordsearch/test.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include "grid.h"
 #include "dictionary.h"
 
 using namespace std;
@@ -9,21 +10,281 @@ using namespace std;
 //this function currently takes in a dictionary object and a filename string.
 //Hopefully the filename corresponds to an input file that's in the same format
 //as the dictionary and if it is, then we can output a file of candidate words:
-void findMatches(dictionary dict, string filename)
+void findMatches(dictionary dict, grid gridIn)
 {
-    cout << "Work in Progress \n";
-    cout << dict;
+    ofstream MyFile("CandidateWords.txt");
 
-    ifstream candidates(filename);      //load file containing candidate words
-    ofstream fout("found-words.txt");   //load file to contain found words
+    string outputWord;  //words to be outputted
 
+    int numRows = gridIn.letters.rows();
+    int numCols = gridIn.letters.cols();
+    int temp;
+    int counter;
+    int temp2;
+    int counterX;
+    int counterY;
+    
+    //start at each character in the grid
+    for (int row = 0; row < numRows; row++)
+    {
+        for (int col = 0; col < numCols; col++)
+        {
+            /*******improved algo for going down right - diagonal*******/
+            outputWord = "";
+
+            counterX = row;
+            counterY = col;
+            counter = 0;
+            
+            while (counter < numRows)
+            {
+                outputWord += gridIn.letters[counterX][counterY];
+
+                counter++;
+                counterX++;
+                counterY++;
+
+                if (counterX == numRows){
+
+                    counterX = 0;
+
+                }
+                    
+
+                if (counterY == numCols){
+
+                    counterY = 0;
+
+                }
+                    
+
+                if (outputWord.size() > 4){
+
+                    cout << outputWord << endl;
+                    MyFile << outputWord<<"\n";
+
+
+                }
+                    
+            }
+            /***********************************************************/
+
+            /*******improved algo for going down left - diagonal********/
+            outputWord = "";
+            
+            counterX = row;
+            counterY = col;
+            counter = 0;
+
+            while (counter < numRows)
+            {
+                outputWord += gridIn.letters[counterX][counterY];
+
+                counter++;
+                counterX++;
+                counterY--;
+
+                if (counterX == numRows)
+                    counterX = 0;
+
+                if (counterY == -1)
+                    counterY = numCols - 1;
+
+                if (outputWord.size() > 4){
+
+                    cout << outputWord << endl;
+                    MyFile << outputWord<<"\n";
+
+
+                }
+            }
+            /***********************************************************/        
+
+            /********improved algo for going up left - diagonal*********/
+            outputWord = "";
+            
+            counterX = row;
+            counterY = col;
+            counter = 0;
+
+            while (counter < numRows)
+            {
+                outputWord += gridIn.letters[counterX][counterY];
+
+                counter++;
+                counterX--;
+                counterY--;
+
+                if (counterX == -1)
+                    counterX = numRows - 1;
+
+                if (counterY == -1)
+                    counterY = numCols - 1;
+                
+                if (outputWord.size() > 4){
+
+                    cout << outputWord << endl;
+                    MyFile << outputWord<<"\n";
+
+
+                }
+            }
+            /***********************************************************/            
+          
+            /********improved algo for going up right -ndiagonal********/
+            outputWord = "";
+            
+            counterX = row;
+            counterY = col;
+            counter = 0;
+
+            while (counter < numRows)
+            {
+                outputWord += gridIn.letters[counterX][counterY];
+
+                counter++;
+                counterX--;
+                counterY++;
+
+                if (counterX == -1)
+                    counterX = numRows - 1;
+
+                if (counterY == numCols)
+                    counterY = 0;
+                
+                if (outputWord.size() > 4){
+
+                    cout << outputWord << endl;
+                    MyFile << outputWord<<"\n";
+
+
+                }
+            }
+            /***********************************************************/
+            
+            /***************improved algo for going right***************/
+            outputWord = "";
+            
+            counterY = col;
+            counter = 0;
+
+            while (counter < numRows)
+            {
+                outputWord += gridIn.letters[counterX][counterY];
+
+                counter++;
+                counterY++;
+
+                if (counterY == numCols)
+                    counterY = 0;
+
+                if (outputWord.size() > 4){
+
+                    cout << outputWord << endl;
+                    MyFile << outputWord<<"\n";
+
+
+                }
+            }
+            /***********************************************************/
+
+            /***************improved algo for going left****************/
+            outputWord = "";
+            
+            counterY = col;
+            counter = 0;
+
+            while (counter < numRows)
+            {
+                outputWord += gridIn.letters[counterX][counterY];
+
+                counter++;
+                counterY--;
+
+                if (counterY == -1)
+                    counterY = numCols - 1;
+
+                if (outputWord.size() > 4){
+
+                    cout << outputWord << endl;
+                    MyFile << outputWord<<"\n";
+
+
+                }
+            }
+            /***********************************************************/
+
+            /****************improved algo for going up*****************/
+            outputWord = "";
+            
+            counterX = row;
+            counter = 0;
+
+            while (counter < numRows)
+            {
+                outputWord += gridIn.letters[counterX][counterY];
+
+                counter++;
+                counterX--;
+
+                if (counterX == -1){
+                    counterX = numRows - 1;
+                    
+                if (outputWord.size() > 4){
+
+                    cout << outputWord << endl;
+                    MyFile << outputWord<<"\n";
+
+
+                }
+            }
+            /***********************************************************/
+
+            /***************improved algo for going down****************/
+            outputWord = "";
+                
+            counterX = row;
+            counter = 0;
+
+            while (counter < numRows)
+            {
+
+                outputWord += gridIn.letters[counterX][counterY];
+
+                counter++;
+                counterX++;
+
+                if (counterX == numRows)
+                    counterX = 0;
+                
+                if (outputWord.size() > 4){
+
+                    cout << outputWord << endl;
+                    MyFile << outputWord<<"\n";
+
+
+                }
+            }
+            cout<<endl;
+            /***********************************************************/
+        }
+        cout << endl;
+    }
+
+    }
+
+    MyFile.close();
+
+    ifstream matching("CandidateWords.txt");
+    ofstream fout("found-words.txt");           //load file to contain found words
+    
     string tmp; //temporary value for comparison
 
-    if(candidates.is_open() && fout.is_open())
+    if(matching.is_open() && fout.is_open())
     {
-        while (!candidates.eof())
+        while (!matching.eof())
         {
-            candidates >> tmp;  //load candidate word into temp variable 
+            matching >> tmp;  //load candidate word into temp variable 
             tmp.append("\n");   //I keep forgetting to append this...
 
             //if the word in 'tmp' is in the dictionary, output it to the file:
@@ -39,18 +300,16 @@ void findMatches(dictionary dict, string filename)
     }
 
     fout.close();
-    candidates.close();
+    matching.close();
 }
 
 int main()
 {
+    grid testGrid("input15.txt");
     dictionary myDict("test-dict.txt");
     myDict.sortDict();
 
-    cout << myDict;
-
-    cout << myDict.searchDict("banana") << endl;
-    cout << myDict.searchDict("pogchampion") << endl;
-
+    findMatches(myDict, testGrid);
+    
     return 0;
 }
